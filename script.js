27ef899f360511b5079aebeb6e2ee934e53ae7f5
KsyKsy-dev <ksy.codedev@gmail.com>
2023-04-19 14:06:57 +0200

    add entries() method to js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// #1 enteries() is ussed to create an iterator that returns an array of a given object's own unumerable property[key, value].
+
+const person = {
+  name: " Mich",
+  secondName: " Scolter",
+  gener: "male",
+};
+const entries = Object.entries(person);
+console.log(entries);
